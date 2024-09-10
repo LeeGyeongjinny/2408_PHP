1e27d83e2def6a27ea7a124b5edff1d0af9dfe16
LeeGyeongjinny <yikj555@gmail.com>
2024-09-10 19:12:24 +0900

    240910 PHP - if

---
+++ PHP/Ex/006_concat.php
@@ -0,0 +1,17 @@
+<?php
+
+// 연결 연산자
+$str1 = "안녕, ";
+$str2 = "PHP!!";
+$concat1 = $str1.$str2;
+
+echo $concat1;
+
+
+// 연결연산자를 이용해서 "안녕하세요~" 라고 출력해 봅시다.
+$str3 = "안녕";
+$str4 = "하세요";
+$num = 1234;
+echo $str3.$str4.$num."~";
+
+// echo $concat2;
