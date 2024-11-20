c200938415f70339909f5900f66dfe31a8d25c68
LeeGyeongjinny <yikj555@gmail.com>
2024-11-20 17:32:58 +0900

    241120 vuestagram

---
+++ side_project/vuestagram/resources/views/components/board/BoardListComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <!-- 리스트 -->
+    <div class="board-list-box">
+        <div @click="openModal" class="item">
+            <img src="/img/spongebob_hello.webp" alt="">
+        </div>
+        <div @click="openModal" class="item">
+            <img src="/img/spongebob_rainbow.webp" alt="">
+        </div>
+        <div @click="openModal" class="item">
+            <img src="/img/spongebob_mic.png" alt="">
+        </div>
+        <div @click="openModal" class="item">
+            <img src="/img/pat_and_mat.jpg" alt="">
+        </div>
+        <div @click="openModal" class="item">
+            <img src="/img/meerkat_what.jpg" alt="">
+        </div>
+        <div @click="openModal" class="item">
+            <img src="/img/panda.jpg" alt="">
+        </div>
+    </div>
+
+    <!-- 상세 모달 -->
+    <div v-show="modalFlg" class="board-detail-box">
+        <div class="item">
+            <img src="/img/spongebob_mic.png" alt="">
+            <hr>
+            <p>내용내용</p>
+            <hr>
+            <div class="etc-box">
+                <span>작성자 : 김아무개</span>
+                <button @click="closeModal">닫기</button>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+
+// -------------------------------------
+// 모달 관련
+const modalFlg = ref(false);
+const openModal = () => {
+    modalFlg.value = true;
+}
+const closeModal = () => {
+    modalFlg.value = false;
+}
+// -------------------------------------
+
+</script>
+
+<style>
+@import url('../../../css/boardList.css');
+</style>
---
+++ side_project/vuestagram/resources/views/components/user/UserInfoComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="user-info-box">
+        <img src="/profile/Pat_and_Mat.jpg">
+        <div class="user-info-content">
+            <h2>유저아이디</h2>
+            <h3>작성글 수 : 999</h3>
+        </div>
+        <router-link to="/board/create"><button class="btn btn-submit">글 작성</button></router-link>
+    </div>
+</template> 
+
+<script setup>
+
+</script>
+
+<style>
+@import url('../../../css/userInfo.css');
+</style>
